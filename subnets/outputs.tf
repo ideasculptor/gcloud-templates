bfb9ce4622d7dfa2b2044936eef5d76e1f86819e
Sam Gendler <sgendler@ignw.io>
2019-10-26 04:40:35 -0700

    Add outputs

--- subnets/outputs.tf
+++ subnets/outputs.tf
@@ -14,3 +14,58 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+output "network_name" {
+  value       = module.subnets.network_name
+  description = "The name of the VPC being modified/created"
+}
+
+output "network_self_link" {
+  value       = module.subnets.network_self_link
+  description = "The URI of the VPC being modified/created"
+}
+
+output "svpc_host_project_id" {
+  value       = module.subnets.svpc_host_project_id
+  description = "Shared VPC host project id."
+}
+
+output "subnets_names" {
+  value       = module.subnets.subnets_names
+  description = "The names of the subnets being created"
+}
+
+output "subnets_ips" {
+  value       = module.subnets.subnets_ips
+  description = "The IPs and CIDRs of the subnets being created"
+}
+
+output "subnets_self_links" {
+  value       = module.subnets.subnets_self_links
+  description = "The self-links of subnets being created"
+}
+
+output "subnets_regions" {
+  value       = module.subnets.subnets_regions
+  description = "The region where the subnets will be created"
+}
+
+output "subnets_private_access" {
+  value       = module.subnets.subnets_private_access
+  description = "Whether the subnets will have access to Google API's without a public IP"
+}
+
+output "subnets_flow_logs" {
+  value       = module.subnets.subnets_flow_logs
+  description = "Whether the subnets will have VPC flow logs enabled"
+}
+
+output "subnets_secondary_ranges" {
+  value       = module.subnets.subnets_secondary_ranges
+  description = "The secondary ranges associated with these subnets"
+}
+
+output "routes" {
+  value       = module.subnets.routes
+  description = "The routes associated with this VPC"
+}
+
