64343d9b5243df604bfc76187885e1bf77218f24
Sam Gendler <sgendler@ignw.io>
2019-10-26 06:58:50 -0700

    initial template skeleton for cloud-sql and gke templates

---
+++ cloud-sql/remote_state.tf
@@ -0,0 +1,32 @@
+# Terragrunt templates to support a reference architecture for gcloud
+# Copyright (C) 2019 Samuel Gendler
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+data "terraform_remote_state" "vpc" {
+  backend = "gcs"
+  config = {
+    bucket = var.terraform_state_bucket
+    prefix = "${var.terraform_state_prefix}/${var.environment}/environment"
+  }
+}
+
+data "terraform_remote_state" "subnets" {
+  backend = "gcs"
+  config = {
+    bucket = var.terraform_state_bucket
+    prefix = "${var.terraform_state_prefix}/${var.environment}/${var.region}/${var.environment}/${var.subnets_path}"
+  }
+}
+
